fb3b5b22f08655f3907cceb48edad5a75055eba2
Ryan Ringler <rringler@gmail.com>
2014-11-22 20:11:47 -0800

    Updates specs to use new RSpec3 syntax

--- spec/google_timezone_result_spec.rb
+++ spec/google_timezone_result_spec.rb
@@ -3,11 +3,11 @@ require 'google_timezone/result'
 describe GoogleTimezone::Result do
   it 'should be success' do
     result = GoogleTimezone::Result.new({ 'status' => 'OK' })
-    result.should be_success
+    expect(result).to be_success
   end
 
   it 'should not be succsess' do
     result = GoogleTimezone::Result.new({ 'status' => 'ZERO_RESULTS' })
-    result.should_not be_success
+    expect(result).to_not be_success
   end
 end
--- spec/google_timezone_spec.rb
+++ spec/google_timezone_spec.rb
@@ -2,29 +2,41 @@ require 'google_timezone'
 
 describe GoogleTimezone do
   describe 'initialize' do
-    before { GoogleTimezone::Base.any_instance.should_receive(:fetch).with(no_args).and_return(GoogleTimezone::Result.new(0)) }
+    context 'without any optional parameters' do
+      let(:result) { GoogleTimezone::Result.new(0) }
+      before {
+        allow_any_instance_of(GoogleTimezone::Base).to(
+          receive(:fetch).with(no_args).and_return(result)
+        )
+      }
 
-    it 'correct with separate lat and lon' do
-      g = GoogleTimezone.fetch(0,0)
-      g.should be_an_instance_of(GoogleTimezone::Result)
-    end
+      it 'should accept separate lat/long paramenters' do
+        g = GoogleTimezone.fetch(0,0)
+        expect(g).to be_an_instance_of(GoogleTimezone::Result)
+      end
 
-    it 'correct with array lat and lon' do
-      g = GoogleTimezone.fetch([0,0])
-      g.should be_an_instance_of(GoogleTimezone::Result)
+      it 'should accept a single lat/long array parameter' do
+        g = GoogleTimezone.fetch([0,0])
+        expect(g).to be_an_instance_of(GoogleTimezone::Result)
+      end
     end
   end
 
   describe '#fetch!' do
-    describe 'if result is not success' do
+    describe 'if result is not successful' do
       let(:result) { GoogleTimezone::Result.new({}) }
+      let(:error)  { GoogleTimezone::Error }
 
-      before { result.stub!(:success?) { false } }
-      before { result.stub!(:result) { 'error message' } }
-      before { GoogleTimezone::Base.any_instance.stub(:fetch) { result } }
+      before { allow(result).to receive(:success?).and_return(false) }
+      before { allow(result).to receive(:result).and_return('error message') }
+      before {
+        allow_any_instance_of(GoogleTimezone::Base).to(
+          receive(:fetch).and_return(result)
+        )
+      }
 
-      it 'should raise error' do
-        expect { GoogleTimezone.fetch!(0,0) }.to raise_error(GoogleTimezone::Error)
+      it 'should raise an error' do
+        expect { GoogleTimezone.fetch!(0,0) }.to raise_error(error)
       end
     end
   end
