9cd2bcc4da4f7ad8eb6146735e8d636652ec5c7f
Olek Janiszewski <olek.janiszewski@gmail.com>
2015-02-13 17:59:00 +0100

    Improve specs
    There were several issues with the specs:
    
    - mocking was misconfigured, so the specs were making actual remote
      requests; we're now mocking only the remote call
    - we now have better coverage for `.fetch` and `.fetch!`

--- spec/google_timezone_spec.rb
+++ spec/google_timezone_spec.rb
@@ -1,23 +1,16 @@
 require 'google_timezone'
 
 describe GoogleTimezone do
+  let(:raw_result) { {} }
+
+  before do
+    allow_any_instance_of(GoogleTimezone::Base).to(
+      receive(:get_result).and_return(raw_result)
+    )
+  end
+
   describe '.fetch' do
     context 'without any optional parameters' do
-      let(:base_instance) { spy }
-      let(:result) { GoogleTimezone::Result.new({}) }
-
-      before do
-        allow_any_instance_of(GoogleTimezone::Base).to(
-          receive(:new).with(any_args)
-                       .and_return(base_instance)
-        )
-
-        allow(base_instance).to(
-          receive(:fetch).with(no_args)
-                         .and_return(result)
-        )
-      end
-
       it 'should accept separate lat/long paramenters' do
         g = GoogleTimezone.fetch(0, 0)
         expect(g).to be_an_instance_of(GoogleTimezone::Result)
@@ -30,25 +23,11 @@ describe GoogleTimezone do
     end
 
     context 'with optional parameters' do
-      let(:base_instance) { spy }
-      let(:result) { GoogleTimezone::Result.new(0) }
       let(:valid_params)   { { timestamp: 1331161200,
                                key: 'A24br8v2w' } }
       let(:invalid_params) { { timestamp: 1331161200,
                                key: 'A24br8v2w',
                                unallowed_param: true } }
-
-      before(:each) do
-        allow_any_instance_of(GoogleTimezone::Base).to(
-          receive(:new).with(0, 0, valid_params)
-                       .and_return(base_instance)
-        )
-
-        allow(base_instance).to(
-          receive(:fetch).with(no_args)
-                         .and_return(result)
-        )
-      end
 
       it 'should pass allowed parameters on to the query' do
         g = GoogleTimezone.fetch(0, 0, valid_params)
@@ -56,6 +35,8 @@ describe GoogleTimezone do
       end
 
       it 'should reject unallowed parameters' do
+        a_hash_excluding = ->(*keys) { satisfy {|actual| (keys & actual.keys).empty? } }
+        expect_any_instance_of(GoogleTimezone::Base).to receive(:get_result).with(a_hash_excluding[:unallowed_param])
         g = GoogleTimezone.fetch(0, 0, invalid_params)
         expect(g).to be_an_instance_of(GoogleTimezone::Result)
       end
@@ -64,19 +45,17 @@ describe GoogleTimezone do
 
   describe '.fetch!' do
     describe 'if result is not successful' do
-      let(:result) { GoogleTimezone::Result.new({}) }
-      let(:error)  { GoogleTimezone::Error }
-
-      before { allow(result).to receive(:success?).and_return(false) }
-      before { allow(result).to receive(:result).and_return('error message') }
-      before {
-        allow_any_instance_of(GoogleTimezone::Base).to(
-          receive(:fetch).and_return(result)
-        )
-      }
-
       it 'should raise an error' do
-        expect { GoogleTimezone.fetch!(0,0) }.to raise_error(error)
+        expect { GoogleTimezone.fetch!(0, 0) }.to raise_error(GoogleTimezone::Error)
+      end
+    end
+
+    describe 'if result is successul' do
+      let(:raw_result) { {'status' => 'OK'} }
+
+      it 'should return a result' do
+        g = GoogleTimezone.fetch!(0, 0)
+        expect(g).to be_an_instance_of(GoogleTimezone::Result)
       end
     end
   end
